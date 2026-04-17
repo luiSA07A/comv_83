0bd8bb60ba489b82f1a6e8274da7560c567d8ff2
luiSA07A <luisairaguevarra7@gmail.com>
2026-04-18 06:20:57 +0800

    Update run_eval.sh

--- scripts/run_eval.sh
+++ scripts/run_eval.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
-#SBATCH --job-name=odelia_eval
-#SBATCH --partition=GPUQ
-#SBATCH --mem=8G
-#SBATCH --time=00:10:00
+#SBATCH --job-name=eval_final
 #SBATCH --output=logs/eval_%j.out
+#SBATCH --partition=CPUQ
+#SBATCH --time=00:10:00
 
-module load Python/3.11.3-GCCcore-12.3.0
 source ~/venvs/odelia/bin/activate
 
-python src/evaluate_results.py
+echo "========================================"
+echo "EVALUATING DENSENET"
+echo "========================================"
+python src/evaluate_results.py --json runs/preds_densenet.json --data_root /cluster/projects/vc/courses/TDT17/mic/ODELIA2025
+
+echo -e "\n========================================"
+echo "EVALUATING MIL"
+echo "========================================"
+python src/evaluate_results.py --json runs/preds_mil.json --data_root /cluster/projects/vc/courses/TDT17/mic/ODELIA2025
